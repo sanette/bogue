8fa7bdeb089b0abfdc2c902a635f6e631daf295b
sanette <sanette-linux@laposte.net>
2019-07-12 10:14:58 +0200

    change version tag, remove ocamlfind from dune-project

--- lib/b_theme.ml
+++ lib/b_theme.ml
@@ -22,7 +22,7 @@ DIR = /home/john/.config/bogue/themes
     
 *)
 
-let this_version = "1907"  (* see VERSION file *)
+let this_version = "20190712"  (* see VERSION file *)
   
 let default_vars = [
   (* Debug: *)
