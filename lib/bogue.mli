eddefeffc04c925fd17d57a5c18dd32759d36553
sanette <sanette-linux@laposte.net>
2022-06-01 11:22:00 +0200

    doc

--- lib/bogue.mli
+++ lib/bogue.mli
@@ -10,7 +10,7 @@
    Bogue is entirely written in {{:https://ocaml.org/}ocaml} except for the
    hardware accelerated graphics library {{:https://www.libsdl.org/}SDL2}.
 
-@version 20220504
+@version 20220601
 
 @author Vu Ngoc San
 
