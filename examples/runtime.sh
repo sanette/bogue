fc4ed23a2469191298a37fb0af4883772b9b8d0f
sanette <sanette-linux@laposte.net>
2023-01-08 18:34:43 +0100

    try to fix alpine's missing RUNTIME_DIR

--- examples/runtime.sh
+++ examples/runtime.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-if test -z "${XDG_RUNTIME_DIR}"; then
-  export XDG_RUNTIME_DIR=/tmp/$(id -u)-runtime-dir
-  if !(test -d "${XDG_RUNTIME_DIR}"); then
-      echo "Creating ${XDG_RUNTIME_DIR}"
-      mkdir "${XDG_RUNTIME_DIR}"
-      chmod 0700 "${XDG_RUNTIME_DIR}"
-  fi
+echo "XDG_RUNTIME_DIR=${XDG_RUNTIME_DIR}"
+
+if test -z "${XDG_RUNTIME_DIR}"
+then
+    export XDG_RUNTIME_DIR=/tmp/$(id -u)-runtime-dir
+    echo "XDG_RUNTIME_DIR=${XDG_RUNTIME_DIR}"
+    if !(test -d "${XDG_RUNTIME_DIR}")
+    then
+	echo "Creating ${XDG_RUNTIME_DIR}"
+	mkdir "${XDG_RUNTIME_DIR}"
+	chmod 0700 "${XDG_RUNTIME_DIR}"
+    fi
 fi
